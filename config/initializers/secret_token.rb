5696c835f845ed2f4ae4a1cfef1914d3f467eefb
Christina <christina.v.stewart@gmail.com>
2013-07-30 15:12:11 -0700

    initial app setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oceanic::Application.config.secret_token = '93a90219479f59884756a85fc143e7ba5a31818c23ec26dae2ceba2cd7657f29c898aabaf15fa79cab9d37d3ac5bce925263b9110ae3685aa04ac8522eacf4ea'
