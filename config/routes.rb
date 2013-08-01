82786767b68e2c99a63bdb05dcd5650b07b86207
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-31 18:25:58 -0700

    Add user controllers and login functionality nnavbar etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Oceanic::Application.routes.draw do
-
-  resources :flights
+  match 'login' => 'user_sessions#new', :as => :login
+  match 'logout' => 'user_sessions#destroy', :as => :logout
 
   get "airports/index"
 
   post "selected_flight" => 'airports#selected_flight'
   get "/departing_flights/:id" => 'airports#departing_flights'
   get "/destination_airport/:origin_airport_id/:destination_airport_id" => 'airports#destination_airport'
+
+  resources :users
+  resources :flights
+  resources :user_sessions, :except => [:index, :show, :edit, :update]
 
   root :to => 'airports#index'
 
